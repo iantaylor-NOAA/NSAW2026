0e11db5f58e6c0493344d5fc29845de9d0b7db0f
iantaylor-NOAA <ian.taylor@noaa.gov>
2026-01-27 07:37:06 -0800

    update fig and code

--- R/old_fish.R
+++ R/old_fish.R
@@ -38,10 +38,29 @@ if (!exists("out_widow")) {
     out_canary <- readRDS("data-raw/out_canary_2023.rds")
 }
 
+# read in older model outputs for comparison
+out_widow_old <- r4ss::SS_output(
+    "C:/SS/Widow/Widow2019/WidowRF_2019_Update/2_base_model",
+    printstats = FALSE,
+    verbose = FALSE
+)
+out_yt_old <- SS_output(
+    "https://raw.githubusercontent.com/pfmc-assessments/yellowtail_2025/refs/heads/main/Model_Runs/1.01_base_2017",
+    printstats = FALSE,
+    verbose = FALSE
+)
+out_canary_old <- r4ss::SS_output(
+    'C:/SS/canary/CanaryRf_2015 base model_run',
+    printstats = FALSE,
+    verbose = FALSE
+)
+
+
 get_old_numbers <- function(out, ref_age) {
     out$natage_annual_2_with_fishery |>
         filter(Bio_Pattern == 1) |>
         select(-Bio_Pattern) |>
+        rename(Sex = any_of("Gender"), Yr = any_of("Year")) |>
         pivot_longer(
             cols = -c(Sex, Yr),
             names_to = "Age",
@@ -61,7 +80,20 @@ olds <- rbind(
         mutate(species = "Yellowtail rockfish"),
     get_old_numbers(out_canary, ref_age = ref_age) |>
         mutate(species = "Canary rockfish")
-)
+) |>
+    mutate(assessment = "new")
+
+# compare old fish numbers from older assessments
+olds2 <- rbind(
+    get_old_numbers(out_widow_old, ref_age = ref_age) |>
+        mutate(species = "Widow rockfish"),
+    get_old_numbers(out_yt_old, ref_age = ref_age) |>
+        mutate(species = "Yellowtail rockfish"),
+    get_old_numbers(out_canary_old, ref_age = ref_age) |>
+        mutate(species = "Canary rockfish")
+) |>
+    mutate(assessment = "old")
+olds <- rbind(olds, olds2)
 
 head(olds)
 #      Yr    N_old species
@@ -80,10 +112,10 @@ olds$color <- case_when(
     TRUE ~ "gray70"
 )
 
-# calculate fraction of old fish in 2023 vs the first
+# calculate frac_old of old fish in 2023 vs the first
 # value in the time series for each species
 olds <- olds |>
-    group_by(species) |>
+    group_by(species, assessment) |>
     mutate(
         first_N_old = first(N_old),
         frac_old = N_old / first_N_old
@@ -94,72 +126,61 @@ olds <- olds |>
 olds <- olds |>
     filter(
         Yr <=
-            if_else(
-                species %in% c("Yellowtail rockfish", "Widow rockfish"),
-                2025,
-                2023
+            case_when(
+                assessment == "new" &
+                    species %in%
+                        c("Yellowtail rockfish", "Widow rockfish") ~ 2025,
+                assessment == "new" & species == "Canary rockfish" ~ 2023,
+                assessment == "old" & species == "Canary rockfish" ~ 2016,
+                assessment == "old" &
+                    species == "Yellowtail rockfish" ~ 2018,
+                assessment == "old" & species == "Widow rockfish" ~ 2020
             )
-    ) |>
-    rename(fraction = frac_old)
+    )
 
 # plot the N_old time series for each species
 library(ggplot2)
-ggplot(olds, aes(x = Yr, y = N_old / 1e6, color = species)) +
-    geom_line(linewidth = 1) +
-    labs(
-        #title = "Estimated Numbers of Old Fish in Three Rockfish Stocks",
-        x = "Year",
-        y = glue::glue("Estimated numbers of fish age {ref_age}+ (millions)"),
-        color = "species"
-    ) +
-    scale_color_manual(
-        values = setNames(olds$color, olds$species)
-    ) +
-    theme_minimal(base_size = 14)
-ggsave(
-    "figures/old_fish_numbers.png",
-    width = 7,
-    height = 5,
-    units = "in",
-    dpi = 300
-)
-
-
-# plot the fraction time series for each species
-ggplot(olds, aes(x = Yr, y = fraction, color = species)) +
-    geom_line(linewidth = 1) +
-    labs(
-        #title = "Estimated Numbers of Old Fish in Three Rockfish Stocks",
-        x = "Year",
-        y = glue::glue(
-            "Estimated numbers of fish age {ref_age}+\nrelative to unfished equlibrium"
-        ),
-        color = "species"
-    ) +
-    scale_color_manual(
-        values = setNames(olds$color, olds$species)
-    ) +
-    theme_minimal(base_size = 14)
-ggsave(
-    "figures/old_fish_relative_numbers.png",
-    width = 7,
-    height = 5,
-    units = "in",
-    dpi = 300
-)
-
-
-# plot the fraction time series for each species
 olds |>
-    dplyr::filter(Yr >= 1980) |>
-    ggplot(aes(x = Yr, y = fraction, color = species)) +
+    #filter(assessment == "new") |>
+    ggplot(aes(
+        x = Yr,
+        y = N_old / 1e6,
+        linetype = assessment,
+        color = species
+    )) +
     geom_line(linewidth = 1) +
     labs(
-        #title = "Estimated Numbers of Old Fish in Three Rockfish Stocks",
-        x = "Year",
-        y = glue::glue(
-            "Estimated numbers of fish age {ref_age}+\nrelative to unfished equlibrium"
+        title = glue::glue(
+            "Estimated numbers of age {ref_age}+ fish\nrelative to unfished equlibrium"
         ),
+        x = "Year",
+        y = "Fraction of unfished equlibrium",
+        color = "species"
+        #linetype = "assessment"
+    ) +
+    scale_color_manual(
+        values = setNames(olds$color, olds$species)
+    ) +
+    theme_minimal(base_size = 14)
+ggsave(
+    "figures/old_fish_numbers2.png",
+    width = 7,
+    height = 5,
+    units = "in",
+    dpi = 300
+)
+
+
+# plot the frac_old time series for each species
+olds |>
+    ggplot(aes(x = Yr, y = frac_old, color = species, linetype = assessment)) +
+    geom_line(linewidth = 1) +
+    labs(
+        title = glue::glue(
+            "Estimated numbers of age {ref_age}+ fish\nrelative to unfished equlibrium"
+        ),
+        x = "Year",
+        y = "Fraction of unfished equlibrium",
         color = "species"
     ) +
     scale_color_manual(
@@ -167,7 +188,33 @@ olds |>
     ) +
     theme_minimal(base_size = 14)
 ggsave(
-    "figures/old_fish_relative_numbers_zoomed.png",
+    "figures/old_fish_relative_numbers2.png",
+    width = 7,
+    height = 5,
+    units = "in",
+    dpi = 300
+)
+
+
+# plot the frac_old time series for each species
+olds |>
+    dplyr::filter(Yr >= 1980 & assessment == "new") |>
+    ggplot(aes(x = Yr, y = frac_old, color = species)) + #, linetype = assessment)) +
+    geom_line(linewidth = 1) +
+    labs(
+        title = glue::glue(
+            "Estimated numbers of age {ref_age}+ fish\nrelative to unfished equlibrium"
+        ),
+        x = "Year",
+        y = "Fraction of unfished equlibrium",
+        color = "species"
+    ) +
+    scale_color_manual(
+        values = setNames(olds$color, olds$species)
+    ) +
+    theme_minimal(base_size = 14)
+ggsave(
+    "figures/old_fish_relative_numbers_zoomed2.png",
     width = 7,
     height = 5,
     units = "in",
@@ -187,7 +234,122 @@ SSplotComparisons(
     legendlabels = c("Widow", "Yellowtail", "Canary"),
     subplots = 3,
     col = c("#000000", "#009E73", "#E69F00"),
-    
 )
 
-#w19 <- r4ss::SS_read('models/2019 base model//Base_45')
+
+# example CAAL plots for WCGBTS fleet by model to get rough estimate of which fish are expected
+# to be large
+
+# yt fleet 6 CAAL at length > 38, max agebin = 30
+# canary fleet 28, CAAL at length > 40, max agebin = 35
+# widow fleet 8, CAAL at length > 35, max agebin = 40
+# averages out to about 70% of Linf
+
+#    Linf <- out$Growth_Parameters |> filter(Sex == 1) |> pull(Linf)
+
+SS_plots(out_canary, fleets = 28, plot = 18, png = FALSE, print = FALSE)
+SS_plots(out_yt, fleets = 6, plot = 18, png = FALSE, print = FALSE)
+SS_plots(out_widow, fleets = 8, plot = 18, png = FALSE, print = FALSE)
+
+# get obs/expected proportion of ages in length bins > 70% of L_at_Amax
+out_canary$Growth_Parameters |> filter(Sex == 1) |> pull(Linf)
+# [1] 59.2228
+out_widow$Growth_Parameters |> filter(Sex == 1) |> pull(Linf)
+# [1] 49.4547
+out_yt$Growth_Parameters |> filter(Sex == 1) |> pull(Linf)
+# [1] 54.4546
+
+get_marginals <- function(out) {
+    fleet <- which(
+        out$FleetNames %in%
+            c("WCGBTS", "28_coastwide_NWFSC", "NWFSC", "NWFSCcombo")
+    )
+    # remove additive constant
+    min <- out$condbase$Obs |> min(na.rm = TRUE)
+    marginals <- out$condbase |>
+        filter(Fleet == fleet & Sex == 1) |>
+        group_by(Yr, Bin) |>
+        summarize(
+            Obs = sum(Nsamp_adj * (Obs - min)),
+            Exp = sum(Nsamp_adj * (Exp - min)),
+            Nsamp_adj = sum(Nsamp_adj)
+        )
+    # rescale within a year to sum to 1
+    for (y in unique(marginals$Yr)) {
+        marginals$Obs[marginals$Yr == y] <- marginals$Obs[marginals$Yr == y] /
+            sum(marginals$Obs[marginals$Yr == y])
+        marginals$Exp[marginals$Yr == y] <- marginals$Exp[marginals$Yr == y] /
+            sum(marginals$Exp[marginals$Yr == y])
+    }
+    return(marginals)
+}
+# # plot Exp vs Bin with colors by year
+# out_widow |> get_marginals() |> ggplot(aes(x = Bin, y = Obs, color = Yr)) + geom_point()
+
+# proportion of CAAL data for females that are > 70% of Linf
+get_old_prop <- function(out, ref_age = 20) {
+    get_marginals(out) |>
+        group_by(Yr) |>
+        # get proportions above ref_age for obs and exp
+        summarize(
+            obs = sum(Obs[Bin >= ref_age]) / sum(Obs),
+            exp = sum(Exp[Bin >= ref_age]) / sum(Exp)
+        ) |>
+        # put in long format better suited to ggplot
+        pivot_longer(
+            cols = c(obs, exp),
+            names_to = "type",
+            values_to = "prop_old"
+        )
+}
+
+x <- out_widow |>
+    get_marginals() |>
+    ungroup() |>
+    filter(Yr == 2005) |>
+    select(Bin, Exp) |>
+    plot()
+x <- out_widow |>
+    get_marginals() |>
+    ungroup() |>
+    filter(Yr == 2006) |>
+    select(Bin, Exp) |>
+    lines()
+x <- out_widow |>
+    get_marginals() |>
+    ungroup() |>
+    filter(Yr == 2007) |>
+    select(Bin, Exp) |>
+    lines()
+
+
+out_widow |>
+    get_old_prop() |>
+    ggplot(aes(x = Yr, y = prop_old, color = type)) +
+    geom_line() #+
+# labs(
+#     title = "Proportion of old fish in CAAL data",
+#     y = glue::glue("Proportion of fish age {ref_age}+")
+# )
+
+ref_age <- 20
+out_yt |>
+    get_old_prop(ref_age = ref_age) |>
+    ggplot(aes(x = Yr, y = prop_old, color = type)) +
+    geom_line() +
+    labs(
+        title = "Proportion of old fish in CAAL data",
+        y = glue::glue("Proportion of fish age {ref_age}+")
+    )
+
+out_yt |>
+    get_old_prop(ref_age = 20) |>
+    filter(Yr >= 2017, type == "exp") |>
+    pull(prop_old) |>
+    mean()
+
+out_yt_old |>
+    get_old_prop(ref_age = 20) |>
+    filter(Yr >= 2014, type == "exp") |>
+    pull(prop_old) |>
+    mean()
